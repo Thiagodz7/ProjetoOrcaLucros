36aefff73f6d88bd731550f764622dd514cd472a
Thiago Rodrigues de S Silva <thiago.sousa@castgroup.com.br>
2021-12-27 15:31:28 -0300

    Add Components Services

--- OrcaLucros_FrontEnd/orcamentosApp/src/app/app.module.ts
+++ OrcaLucros_FrontEnd/orcamentosApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,11 +11,12 @@ import { OrcamentosComponent } from './orcamentos/orcamentos.component';
   declarations: [
     AppComponent,
     HomeAppComponent,
-    OrcamentosComponent
+    OrcamentosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
